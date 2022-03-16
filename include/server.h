019958d582096da31c99b56f14af720e28f2908a
shahnikba <nikbakhtian@gmail.com>
2022-03-16 12:00:48 +0000

    final update

--- include/server.h
+++ include/server.h
@@ -18,7 +18,7 @@ public:
     std::shared_ptr<Client> add_client(std::string id);
     std::shared_ptr<Client> get_client(std::string id) const;
     double get_wallet(std::string id) const;
-    bool parse_trx(std::string trx, std::string* sender, std::string* receiver, double* value) const;
+    static bool parse_trx(std::string trx, std::string& sender, std::string& receiver, double& value);
     bool add_pending_trx(std::string trx, std::string signature);
     size_t mine();
 
